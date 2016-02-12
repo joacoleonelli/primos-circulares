e9f576f4737b152b4ca58f398564cfa14c1a2c5b
joacoleonelli <joacoleonelli@gmail.com>
2016-02-11 21:40:55 -0300

    Agregando hilo para calculo de primos

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,23 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		CalculosMatematicos calculos = new CalculosMatematicos();
-		int contador = 0;
-		for(int n = 1; n <= 1000000; n++){
-			if(calculos.esPrimoCircular(n)){
-				System.out.println(n);
-				contador++;
-			}
-		}
-		System.out.println("Cantidad de números primos circulares de uno al millon: " + contador);
+//		CalculosMatematicos calculos = new CalculosMatematicos();
+//		int contador = 0;
+//		for(int n = 1; n <= 1000000; n++){
+//			if(calculos.esPrimoCircular(n)){
+//				System.out.println(n);
+//				contador++;
+//			}
+//		}
+//		System.out.println("Cantidad de números primos circulares de uno al millon: " + contador);}
+		
+        CalculosMatematicos cm = new CalculosMatematicos();
+
+        ThreadNumerosPrimos threadNP = null;
+//        ThreadPrimosCirculares threadNPCirc = null;
+
+        /* Creo un nuevo Thread que será para armar la lista de numeros primos*/
+        threadNP = new ThreadNumerosPrimos("Calculo de Numeros Primos", cm);
+        threadNP.start();
 	}
 }
